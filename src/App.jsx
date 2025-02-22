6ede4681097ed016d288401da08d8b4cbbe3bdce
ChatGavin <0xduer@gmail.com>
2025-02-23 01:55:20 +0800

    feat: Add global styles and update App component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Designer from "@/pages/designer";
 import "normalize.css";
 import "antd/dist/reset.css";
+// 设计器
+import Designer from "@/pages/designer";
 
 function App() {
   return <Designer />;
