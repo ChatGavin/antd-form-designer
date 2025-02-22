4595ba63d02de310782bd86e8db337f9c0f4460b
ChatGavin <0xduer@gmail.com>
2025-02-23 03:04:40 +0800

    feat: Add Tailwind CSS support to project configuration

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,6 @@
 // 第三方样式库
 import "normalize.css";
+import "./tailwind.css";
 import "antd/dist/reset.css";
 // 设计器样式
 import "./designer.css";
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/", // 如果是部署到 github pages，需要设置为 /<repo-name>/
   resolve: {
     alias: {
