dca10463d970dffc57bbd88965645941d26c8b2a
ChatGavin <0xduer@gmail.com>
2025-02-23 10:42:53 +0800

    refactor: Replace Antd Layout Sider with custom div in LeftPanel

--- src/pages/designer/LeftPanel/index.jsx
+++ src/pages/designer/LeftPanel/index.jsx
@@ -1,15 +1,12 @@
-import { Layout } from "antd";
 import styles from "./index.module.css";
 import { TabNav, TabContent } from "./index.js";
 
-const { Sider } = Layout;
-
 const App = () => {
   return (
-    <Sider className={styles.sider} width={200} theme="light">
+    <div className={styles.layout}>
       <TabNav />
       <TabContent />
-    </Sider>
+    </div>
   );
 };
 
