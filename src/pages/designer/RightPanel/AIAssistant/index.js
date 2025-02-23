677664acde189a67ad2bf48774f2ac49b0e493a4
ChatGavin <0xduer@gmail.com>
2025-02-24 00:09:57 +0800

    chore: Remove AIAssistant components from designer RightPanel

--- src/pages/designer/RightPanel/AIAssistant/index.js
+++
@@ -1,6 +0,0 @@
-// 导出组件
-export { default as Menu } from "./Menu";
-export { default as Chat } from "./Chat";
-export { default as Help } from "./Help";
-// 导出默认组件
-export { default } from "./index.jsx";
