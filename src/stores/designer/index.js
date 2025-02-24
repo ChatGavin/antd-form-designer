2813a2dc3c620ca6e2be1e4fcb86fabd20cb13d7
ChatGavin <0xduer@gmail.com>
2025-02-24 22:38:07 +0800

    feat: Integrate StepsStore into DesignerStore

--- src/stores/designer/index.js
+++ src/stores/designer/index.js
@@ -1,17 +1,24 @@
 import { makeAutoObservable } from "mobx";
+// dialog
 import DialogStore from "./dialog";
+// leftPanel
 import { TabsStore } from "./leftPanel";
+// middleContent
+import { StepsStore } from "./middleContent";
 
 class DesignerStore {
   tabsStore;
   dialogStore;
+  stepsStore;
 
   constructor() {
     this.dialogStore = new DialogStore();
     this.tabsStore = new TabsStore();
+    this.stepsStore = new StepsStore();
     makeAutoObservable(this, {
       dialogStore: false, // 标记为非观察对象，因为它本身就是一个 observable
       tabsStore: false,
+      stepsStore: false,
     });
   }
 }
