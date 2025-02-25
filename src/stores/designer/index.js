3bf15a89b24bd7711a6e4b168894370a93dd7bf8
ChatGavin <0xduer@gmail.com>
2025-02-26 01:26:52 +0800

    feat: Add ToolbarStore for managing right panel tabs

--- src/stores/designer/index.js
+++ src/stores/designer/index.js
@@ -5,20 +5,25 @@ import DialogStore from "./dialog";
 import { TabsStore } from "./leftPanel";
 // middleContent
 import { StepsStore } from "./middleContent";
+// rightPanel
+import ToolbarStore from "./rightPanel/toolbarStore";
 
 class DesignerStore {
   tabsStore;
   dialogStore;
   stepsStore;
+  toolbarStore;
 
   constructor() {
     this.dialogStore = new DialogStore();
     this.tabsStore = new TabsStore();
     this.stepsStore = new StepsStore();
+    this.toolbarStore = new ToolbarStore();
     makeAutoObservable(this, {
       dialogStore: false, // 标记为非观察对象，因为它本身就是一个 observable
       tabsStore: false,
       stepsStore: false,
+      toolbarStore: false,
     });
   }
 }
