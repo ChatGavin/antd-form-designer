06810392b93ad0090f8e898cd1969820c0a98669
ChatGavin <0xduer@gmail.com>
2025-03-01 01:10:50 +0800

    refactor: 删除设计器页面的多个组件和样式文件

--- src/pages/designer/RightPanel/ToolbarContent/index.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./index.module.css";
-import { observer } from "mobx-react-lite";
-
-const App = observer(() => {
-  return <div className={styles.container}>内容</div>;
-});
-
-export default App;
