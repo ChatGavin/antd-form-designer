a8589f0ddf68b7f852b6f773b7ecb18977512bd8
ChatGavin <0xduer@gmail.com>
2025-03-01 00:23:41 +0800

    refactor: 更新设计器模块导入路径为相对路径

--- src/pages/designer/MiddleContent/index.js
+++ src/pages/designer/MiddleContent/index.js
@@ -1,6 +1,6 @@
 // 导出组件
-export { default as Steps } from "./Steps";
-export { default as Workspace } from "./Workspace";
-export { default as Footer } from "./Footer";
+export { default as Steps } from "./Steps/index.jsx";
+export { default as Workspace } from "./Workspace/index.jsx";
+export { default as Footer } from "./Footer/index.jsx";
 // 导出 MiddleContent 组件
 export { default } from "./index.jsx";
--- src/pages/designer/header/index.js
+++ src/pages/designer/header/index.js
@@ -1,4 +1,4 @@
 // 导出组件
-export { default as Brand } from "./Brand";
+export { default as Brand } from "./Brand/index.jsx";
 // 导出 Header 组件
 export { default } from "./index.jsx";
