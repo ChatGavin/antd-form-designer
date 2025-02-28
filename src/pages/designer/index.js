8ecdd408784167a5e9331d0d1f979e6768349b43
ChatGavin <0xduer@gmail.com>
2025-03-01 00:14:24 +0800

    refactor: 更新设计器模块导入路径为绝对路径

--- src/pages/designer/index.js
+++ src/pages/designer/index.js
@@ -1,7 +1,7 @@
 // 导出设计器主模块组件
-export { default as Header } from "./Header";
-export { default as LeftPanel } from "./LeftPanel";
-export { default as MiddleContent } from "./MiddleContent";
-export { default as RightPanel } from "./RightPanel";
+export { default as Header } from "@/pages/designer/Header";
+export { default as LeftPanel } from "@/pages/designer/LeftPanel";
+export { default as MiddleContent } from "@/pages/designer/MiddleContent";
+export { default as RightPanel } from "@/pages/designer/RightPanel";
 // 导出 Designer 组件
-export { default } from "./index.jsx";
+export { default } from "@/pages/designer/index.jsx";
