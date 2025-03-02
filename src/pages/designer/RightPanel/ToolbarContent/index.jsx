0301b4abd2ac769a71fea44f01ad85bdb9d1de1d
ChatGavin <0xduer@gmail.com>
2025-03-02 10:58:40 +0800

    fix: 更新工具栏内容组件的文本描述

--- src/pages/designer/RightPanel/ToolbarContent/index.jsx
+++ src/pages/designer/RightPanel/ToolbarContent/index.jsx
@@ -2,7 +2,7 @@ import styles from "./index.module.css";
 import { observer } from "mobx-react-lite";
 
 const App = observer(() => {
-  return <div className={styles.container}>内容</div>;
+  return <div className={styles.container}>内容区</div>;
 });
 
 export default App;
